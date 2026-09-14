2cce3aa1f1303c234f7a6a0a198760b94213c077
quendae <qnd.000@gmail.com>
2026-09-14 09:24:49 +0200

    test(runtime): expect LoRA-enabled app server

--- tests/StableAmd.Launcher.Tests.ps1
+++ tests/StableAmd.Launcher.Tests.ps1
@@ -25,7 +25,7 @@ Describe 'StableAMD one-click launcher' {
         $script = Get-Content $launcherPath -Raw
 
         $script | Should -Match 'Start-StableAMD\.ps1'
-        $script | Should -Match 'stableamd_server\.py'
+        $script | Should -Match 'stableamd_v03_lora_server\.py'
         $script | Should -Match 'TheRockPython'
         $script | Should -Match '--host\s+127\.0\.0\.1'
         $script | Should -Match '/api/health'
@@ -88,6 +88,7 @@ Describe 'StableAMD one-click launcher' {
         $stop | Should -Match 'AppStatePath'
         $stop | Should -Match 'Get-CimInstance\s+Win32_Process'
         $stop | Should -Match 'stableamd_server'
+        $stop | Should -Match 'stableamd_v03_lora_server'
         $stop | Should -Match 'run_comfy_isolated'
         $stop | Should -Match 'taskkill\.exe'
         $stop | Should -Match '/T /F'
